dc01841355016185ff5d7f8e57bd82c57dee9310
Rajesh Kumar <rajesh.kumar@cred.club>
2020-11-26 17:25:00 +0530

    initial commit

---
+++ src/main/java/com/unilogcorp/bookcasestudy/BookcasestudyApplication.java
@@ -0,0 +1,13 @@
+package com.unilogcorp.bookcasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcasestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcasestudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unilogcorp/bookcasestudy/BookcasestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unilogcorp.bookcasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcasestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
